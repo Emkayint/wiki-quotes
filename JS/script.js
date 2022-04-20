5188e472a1809f1b9f3e9dde86916c5f7c17dab9
Martin <mkmartiones@gmail.com>
2022-04-20 22:16:30 +0300

    st: nav buttons

--- JS/script.js
+++ JS/script.js
@@ -21,6 +21,10 @@ function getApiData(param){
 
 // getApiData(fetchData)
 
-le
+let btn = document.querySelectorAll('button')
 
-
+btn.forEach(button => button.addEventListener('click', () => {
+    if(button.innerText === 'Quotes'){
+        document.getElementById('quote-section').style.display = "none"
+    }
+}))
